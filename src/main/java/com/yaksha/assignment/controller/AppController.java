babd7bef15aa45f6a063ed8de664d3a491ef8778
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-03-31 07:16:56 +0530

    Ameet - Update AppController.java
    Update AppController.java for template

--- src/main/java/com/yaksha/assignment/controller/AppController.java
+++ src/main/java/com/yaksha/assignment/controller/AppController.java
@@ -8,23 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
 public class AppController {
-
-	private final RestTemplate restTemplate;
-
-	public AppController(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
-	}
-
-	// PUT request to update resource
-	@PutMapping("/updateResource")
-	public String sendPutRequest(@RequestParam String apiUrl, @RequestBody String requestData) {
-		// Sending the PUT request to the external API
-		HttpEntity<String> entity = new HttpEntity<>(requestData);
-		String response = restTemplate.exchange(apiUrl, HttpMethod.PUT, entity, String.class).getBody();
-
-		// Return the response from the external API, along with the sent data
-		return "Data to update: " + requestData + "\nResponse from API: " + response;
-	}
+	// write your logic here
 }
